f9ce9f7fe222f30da514b421b0375aa7859449ce
TaigoKuriyama <t.lorinza@gmail.com>
2020-03-21 09:32:32 +0900

    ac

--- problem/abc145/c/main.py
+++ problem/abc145/c/main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-S = input()
-N = int(input()) 
-S = input().split()
-A, B, C = input().split()
-L = list(map(int, input().split()))
-H, N = map(int, input().split())
+import itertools
+import math
+n = int(input())
+x = [0] * n
+y = [0] * n
+l = list(range(n))
+ans = 0
+for i in range(n):
+    x[i], y[i] = map(int, input().split())
+for v in itertools.permutations(l, 2):
+    ans += math.sqrt((x[v[0]] - x[v[1]])**2 + (y[v[0]] - y[v[1]])**2)
+print(ans/n)
