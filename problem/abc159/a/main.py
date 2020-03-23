cc4a65d77abc9c3b6edc460b383bf8da1919f037
TaigoKuriyama <t.lorinza@gmail.com>
2020-03-23 21:22:36 +0900

    update

--- problem/abc159/a/main.py
+++ problem/abc159/a/main.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python3
-import math
-def comb(n, r):
-    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
-base = comb(5,3)
 m, n = map(int, input().split())
-if m > 1 and n > 1:
-    ans = comb(n, 2) + comb(m, 2)
-elif n > 1:
-    ans = comb(n, 2) 
-elif m > 1:
-    ans = comb(m, 2)
-else:
-    ans = 0
-print(ans)
+print((m * (m-1) + n * (n - 1)) // 2)
