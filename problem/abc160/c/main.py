88b847e9a6422037989786d356560b583342445c
TaigoKuriyama <t.lorinza@gmail.com>
2020-03-28 23:09:56 +0900

    ac

--- problem/abc160/c/main.py
+++ problem/abc160/c/main.py
@@ -3,6 +3,6 @@ k, n = map(int, input().split())
 a = list(map(int, input().split()))
 l = []
 for i in range(n-1):
-    l.append(a[i+1] - a[i])
+    l.append(a[i+1]-a[i])
 l.append(k-a[-1]+a[0])
 print(k-max(l))
