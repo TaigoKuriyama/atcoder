97ca171c03ac88b671959830214c95db29572008
TaigoKuriyama <t.lorinza@gmail.com>
2020-04-05 11:02:52 +0900

    ac

--- problem/abc161/d/main.py
+++ problem/abc161/d/main.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 k = int(input())
-n = 10 ** 10000
-s = 0
-for i in range(1, n):
-    i = str(i)
-    if len(i) == 1:
-        s += 1
-    if s == k:
-        print(i)
+a = []
+for i in range(1, 10):
+    a.append(i)
+while True:
+    if k <= len(a):
+        print(a[k - 1])
         exit()
-    for t in range(0, len(i) - 1):
-        if abs(int(i[t]) - int(i[t + 1])) > 1:
-            break
-    else:
-        s += 1
-        if s == k:
-            print(i)
-            exit()
+    k -= len(a)
+    old = []
+    a, old = old, a
+    for x in old:
+        for i in range(-1, 2):
+            d = x % 10 + i
+            if d < 0 or d > 9:
+                continue
+            nx = x * 10 + d
+            a.append(nx)
