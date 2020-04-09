8994d97e3507d47285caedd29c5bdccab3ccf22d
TaigoKuriyama <t.lorinza@gmail.com>
2020-04-09 18:31:22 +0900

    ac

---
+++ problem/abc126/c/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+s = input()
+n = int(input()) 
+s = input().split()
+a, b, c = input().split()
+l = list(map(int, input().split()))
+h, n = map(int, input().split())
+s = [input() for _ in range(H)]
