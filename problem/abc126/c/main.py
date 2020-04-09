3e7a05115cb4ebc2f7a62fa3a60534ccf7408e3d
TaigoKuriyama <t.lorinza@gmail.com>
2020-04-09 21:47:40 +0900

    ac

--- problem/abc126/c/main.py
+++ problem/abc126/c/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-s = input()
-n = int(input()) 
-s = input().split()
-a, b, c = input().split()
-l = list(map(int, input().split()))
-h, n = map(int, input().split())
-s = [input() for _ in range(H)]
+n, k = map(int, input().split())
+ans = 0
+for p in range(1, n + 1):
+    c = 0
+    while (True):
+        if p >= k:
+            ans += (1 / n) * (1 / 2) ** c
+            break
+        p *= 2
+        c += 1
+print(ans)
