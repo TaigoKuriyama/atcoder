b3db701321ed7d2c86c801c70f4d2c78103c034f
TaigoKuriyama <t.lorinza@gmail.com>
2020-05-02 08:56:34 +0900

    change

--- problem/abc113/b/main.py
+++ problem/abc113/b/main.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-import numpy as np
 n = int(input()) 
 t, a = map(int, input().split())
-h = list(map(int, input().split()))
-a_t = []
-for i in h:
-    a_t.append(t - i * 0.006)
-print(np.abs(np.asarray(a_t) - a).argmin() + 1)
+h = [abs(t - x * 0.006 - a) for x in map(int, input().split())]
+print(h.index(min(h)) + 1)
