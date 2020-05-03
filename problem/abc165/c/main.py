7c49243605d8eb4bb26a3a7e3ade4b149bc9a04a
TaigoKuriyama <t.lorinza@gmail.com>
2020-05-03 17:02:43 +0900

    ac

--- problem/abc165/c/main.py
+++ problem/abc165/c/main.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python3
+import itertools
 n, m, q = map(int, input().split())
-dic = {}
-for _ in range(q):
-    a, b, c, d = map(int, input().split())
-    dic[(a, b), c] = d
-dic = dict(sorted(dic.items(), key=lambda x: x[1], reverse=True))
+abcd = [tuple(map(int, input().split())) for _ in range(q)]
 ans = 0
-tmp = {}
-print(dic)
-for k in dic:
-    print("k[0]: ", k[0])
-    print("k[1]: ", k[1])
-    print("tmo", tmp)
-    if k[0] not in tmp:
-        print("1", k)
-        ans += dic[k]
-        tmp[k[0]] = k[1]
-    elif k[0] in tmp and k[0][0] == tmp[k[0]]:
-        print("2", k)
-        ans += dic[k]
+for com in itertools.combinations_with_replacement(range(1, m + 1), n):
+    score = sum([d for a, b, c, d in abcd if com[b - 1] - com[a - 1] == c])
+    ans = max(ans, score)
 print(ans)
