63f4d986ff24b37bc8bc52fc21e0f3af3a708a3d
TaigoKuriyama <t.lorinza@gmail.com>
2020-05-05 18:56:01 +0900

    ac

---
+++ problem/abc106/b/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+n = int(input()) 
+def make_divisors(n):
+    div = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            div.append(i)
+            if i != n // i:
+                div.append(n//i)
+    return div
+ans = 0
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        div = make_divisors(i)
+        if len(div) == 8:
+            ans += 1
+print(ans)
