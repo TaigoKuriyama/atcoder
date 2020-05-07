95f2d1adafd00086cb4399aa5e6eebee372bcc78
TaigoKuriyama <t.lorinza@gmail.com>
2020-05-07 22:12:54 +0900

    ac

---
+++ problem/abc105/b/main.py
@@ -0,0 +1,9 @@
+n = int(input())
+cmax = 100 // 4
+dmax = 100 // 7
+for i in range(0, cmax + 1):
+    for j in range(1, dmax + 1):
+        if i + 4 + j * 7 == n:
+            print("Yes")
+            exit()
+print("No")
