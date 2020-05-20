b5583c30a425082fc4e0bd508800b2c164d12b29
TaigoKuriyama <t.lorinza@gmail.com>
2020-05-20 21:52:40 +0900

    ac

---
+++ problem/abc102/a/main.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+n = int(input()) 
+print(n) if n % 2 == 0 else print(n * 2)
---
+++ problem/abc102/b/main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+n = int(input()) 
+a = list(map(int, input().split()))
+print(abs(max(a) - min(a)))
